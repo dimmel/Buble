5e6f03de481910a6cc859d25348e1908ca06ad95
Dima <dim_mel@i.ua>
2018-04-27 12:44:45 +0300

    Залито

---
+++ app/src/main/java/com/buble/ICanvasView.java
@@ -0,0 +1,11 @@
+package com.buble;
+
+public interface ICanvasView {
+    void drawCircle(SimpleCircle circle);
+
+    void redraw();
+
+    void showMessage(String text);
+
+    //void redraw();
+}
